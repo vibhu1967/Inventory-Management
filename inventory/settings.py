1f21306f77316217f3923f5b950b1064a1eb67bc
vaibhav <105483646+vibhu1967@users.noreply.github.com>
2022-09-06 16:37:38 +0530

    Update settings.py

--- inventory/settings.py
+++ inventory/settings.py
@@ -140,4 +140,6 @@ LOGIN_REDIRECT_URL = 'factories-home'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://vaibhavback-end.azurewebsites.net'
+]
